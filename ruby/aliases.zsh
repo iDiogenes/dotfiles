2a9b050498b12b3780a3ca18ef10e92ea705a564
JD Trout <jdtrout@ucla.edu>
2013-01-04 11:05:58 -0800

    Fixed a few issues that bugged me.

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,7 +1,7 @@
-alias r='rbenv local 1.9.3-p194'
+#alias r='rbenv local 1.9.3-p194'
 
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
 
-alias migrate='rake db:migrate db:test:clone'
+#alias migrate='rake db:migrate db:test:clone'
