ccbd18ce1d4cbf930f0b5eed817fb0f034f8f9b7
JD Trout <jdtrout@ucla.edu>
2015-06-25 17:14:21 -0700

    Added docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,7 +1,10 @@
 # Docker config
-export DOCKER_CERT_PATH=/Users/jdtrout/.docker/machine/machines/dev
-export DOCKER_HOST=$(docker-machine url)
-export DOCKER_TLS_VERIFY=1
+docker-machine ip > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  export DOCKER_CERT_PATH=/Users/jdtrout/.docker/machine/machines/dev
+  export DOCKER_HOST=$(docker-machine url)
+  export DOCKER_TLS_VERIFY=1
+fi
 
 # Docker aliases
 alias d='docker'
